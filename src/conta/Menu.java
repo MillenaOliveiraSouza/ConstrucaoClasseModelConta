bdc2044ac372279d1ff5919992aa796d5c761aff
MillenaOliveiraSouza <millena10.oliveira@gmail.com>
2024-02-19 14:34:23 -0300

    Construção da classe model conta

---
+++ src/conta/Menu.java
@@ -0,0 +1,26 @@
+package conta;
+
+import conta.model.Conta;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Conta c1 = new Conta(1, 123, 1, "Sabrina", 250000.0f);
+
+		c1.visualizar();
+		c1.setAgencia(456);
+		//System.out.println(c1.getAgencia());
+
+		if (c1.sacar(100))
+			System.out.println("Saque efetuado com sucesso. O novo saldo é de: " + c1.getSaldo());
+		else
+			System.out.println("O saldo é insuficiente!");
+
+		c1.depositar(1000);
+		System.out.println("O novo saldo é: " + c1.getSaldo());
+
+	}
+
+}
